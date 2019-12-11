8261838b924384a948545e6981a4eeebf8b1ec56
ceifa <gabrinelson27@gmail.com>
2019-12-11 19:53:45 -0300

    Improve helper scripts

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,5 @@
+# BUILD IMAGE
 docker build . -t ceifa/gmod-server
+
+# WAIT FOR INPUT, USEFUL TO SEE UNEXPECTED ERRORS
 read
--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,24 @@
-docker stop gmod-server -t 0
-docker rm gmod-server
-docker run --net=host -it --name gmod-server ceifa/gmod-server
+NAME="gmod-server"
+
+# CHECK IF CONTAINER ALREADY EXISTS
+if [ "$(docker ps -a -q -f name="${NAME}" --format '{{.Names}}')" == "${NAME}" ]; then
+    # CHECK IF THE CONTAINER IS RUNNING
+    if [ "$(docker ps -q -f name="${NAME}" --format '{{.Names}}')" == "${NAME}" ]; then
+        docker stop "${NAME}" -t 0
+    fi
+
+    docker rm "${NAME}"
+fi
+
+# START CONTAINER
+docker run \
+    -p 27015:27015/udp \
+    -p 27015:27015 \
+    -p 27005:27005 \
+    -p 27005:27005/udp \
+    -it \
+    --name gmod-server \
+    ceifa/gmod-server
+
+# WAIT FOR INPUT, USEFUL TO SEE UNEXPECTED ERRORS
 read
